bce2c5aa6ba8528c42ee6b28b722407789336a14
Berkay Cetin <berkaycetin1710@gmail.com>
2021-11-25 23:26:26 +0300

    Done !

---
+++ exe/copyingFromArrayToArrayReverseStack.asm
@@ -0,0 +1,51 @@
+; multi-segment executable file template.
+
+data segment   
+    
+     name1 db 'Berkay'
+     reverseName1 db 6 dp(?)
+    
+ends
+
+stack segment
+    dw   128  dup(0)
+ends
+
+code segment
+start:
+; set segment registers:
+    mov ax, data
+    mov ds, ax
+
+    
+    lea bx,name1                  
+                  
+    mov si,0 
+    mov di,5             
+    mov cx,6
+    
+loop1:
+
+    push [bx + si]
+    inc si
+    
+    pop ax
+    mov reverseName1[di] , al
+    dec di
+    
+    
+loop loop1
+     
+    
+    
+        
+    mov ax, 4c00h ; exit to operating system.
+    int 21h    
+ends
+
+end start ; set entry point and stop the assembler.
+
+
+
+;name1 deki butun elemanlari pushla loop kullanarak
+;reverseName1 e baska bir loop kullanarak pop et
